0b0673a95dc2d78019d1828237af4ea8407a04e7
Brian <bgswilde@gmail.com>
2021-08-16 00:39:05 -0500

    added questions, license info, markdown template

--- index.js
+++ index.js
@@ -1,13 +1,94 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const askQuestions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project? (Required)',
+      validate: title => {
+        if (title) {
+          return true;
+        } else {
+          console.log('Please enter your project title!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please describe your project, as you would want it to read in your READme.md file. (Required)',
+      validate: description => {
+        if (description) {
+          return true;
+        } else {
+          console.log('Please enter a description!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'What steps are required to install your application?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: "What instructions would you like to give for the usage of this application?",
+    },
+    {
+      type: 'input',
+      name: 'deployed',
+      message: 'What is your deployed application link? (If none, leave blank)',
+    },
+    {
+      type: 'input',
+      name: 'screenshot',
+      message: "If you have a screenshot, include the link here. (relative path works best)",
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Who would you like credit in the development of this project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license would you like to use for this project?',
+      choices: [ 'MIT', 'Apache', 'Unlicense', 'GPL', 'MPL' ]
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What guidelines would you like to give for contributing?'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What instructions would you like to give for tests?'
+    },
+    {
+      type: 'input',
+      name: 'contact',
+      message: 'If anybody should have questions, please enter an email address or alternate contact method here:'
+    },
+])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,104 @@
+const { MIT, Apache, GPL, MPL, Unlicense } = require('./utils/licenses.js');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'MIT':
+      badge = '[License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+      break;
+    case 'Apache':
+      badge = '[License: Appache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+      break;
+    case 'GPL':
+      badge = '[License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+      break;
+    case 'MPL':
+      badge = '[License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)';
+      break;
+    case 'Unlicense':
+      badge = '[License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+      licenseLink = '(https://opensource.org/licenses/MIT)';
+      break;
+    case 'Apache':
+      licenseLink = '(https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'GPL':
+      licenseLink = '(https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case 'MPL':
+      licenseLink = '(https://opensource.org/licenses/MPL-2.0)';
+      break;
+    case 'Unlicense':
+      licenseLink = '(http://unlicense.org/)';
+      break;
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch(license) {
+    case 'MIT':
+      licenseText = MIT;
+      break;
+    case 'Apache':
+      licenseText = Apache;
+      break;
+    case 'GPL':
+      licenseText = GPL;
+      break;
+    case 'MPL':
+      licenseText = MPL;
+      break;
+    case 'Unlicense':
+        licenseText = Unlicense;
+        break;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `
+  # ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](##Usage)
+  * [Credits](##Credits)
+  * [License](##License)
+  * [Contributing] (##Contributing)
+  * [Tests] (##Tests)
+  * [Questions](#Questions)
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ### Deployed link and screenshot (if applicable)
+  ${data.deployed}
+  ${data.screenshot}
+  ## Credits
+  ${data.credits}
+  ## License
+  [!${renderLicenseBadge(data.license).badge}]${renderLicenseLink(data.license).licenseLink}
+  ${renderLicenseSection(data.license).licenseText}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  If you have any questions, please contact me here:
+  ${data.contact}
+`
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown, renderLicenseBadge, renderLicenseLink, renderLicenseSection }
